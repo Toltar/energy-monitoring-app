749a0ff0bf8a1169a5eff99bd0e6d8aa042df197
Toltar <matthew@matthewdickens.com>
2025-01-11 16:42:24 -0800

    Implement manual input lambda

--- src/handlers/energy/input-handler.ts
+++ src/handlers/energy/input-handler.ts
@@ -1,10 +1,72 @@
 import { APIGatewayEvent, APIGatewayProxyHandler } from 'aws-lambda';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { PutCommand, DyanmoDBDocumentClient, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
+const ENERGY_USEAGE_TABLE = process.env.ENERGY_USEAGE_TABLE!;
+
+interface EnergyData {
+  date: string;
+  usage: number;
+}
+
+// TODO: Add more errors along the lines of invalid 
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent) => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: JSON.stringify({ message: `Hello, world from ${event.path}` }),
-  };
+  try {
+    if (!event.body) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Missing request body' })
+      };
+    }
+
+    const authorizer = event.requestContext.authorizer;
+
+    const userId: string = authorizer?.claims.sub;
+
+    if (!userId) {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({ message: 'Unauthorized' })
+      };
+    }
+
+
+    const data: EnergyData = JSON.parse(event.body);
+
+    if (!data.date || !data.usage || isNaN(data.usage)) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Invalid input data' }),
+      };
+    }
+
+    const putCommand: PutCommand = new PutCommand({
+      TableName: ENERGY_USEAGE_TABLE,
+      Item: {
+        userId: userId,
+        date: data.date,
+        usage: data.usage,
+        timestamp: new Date().toISOString()
+      },
+    });
+
+    await docClient.send(putCommand);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Energy data saved successfully'
+      })
+    };
+  } catch (_) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Internal server error'
+      })
+    }
+  }
 }
