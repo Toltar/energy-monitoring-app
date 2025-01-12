88d64a3bcf0fe2d6b024c348d1c231fd67e98ba3
Toltar <matthew@matthewdickens.com>
2025-01-11 16:42:08 -0800

    Implement auth lambdas

--- src/handlers/auth/signin.ts
+++ src/handlers/auth/signin.ts
@@ -1,11 +1,80 @@
+import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
 import { APIGatewayEvent, APIGatewayProxyHandler } from 'aws-lambda';
+import { emailRegex } from './util';
 
+const cognito = new CognitoIdentityProvider();
+const CLIENT_ID = process.env.USER_POOL_CLIENT_ID!;
+interface SignInRequest {
+  email: string;
+  password: string;
+}
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent) => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: JSON.stringify({ message: `Hello, world from ${event.path}` }),
-  };
+  try {
+    if (!event.body) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Missing request body' })
+      };
+    }
+
+    const { email, password }: SignInRequest = JSON.parse(event.body);
+
+    if (!email || !password || !emailRegex.test(email)) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Missing required fields' })
+      };
+    }
+
+    if (!emailRegex.test(email)) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: `Invalid email of: ${email}` })
+      };
+    }
+
+    const { AuthenticationResult } = await cognito.initiateAuth({
+      AuthFlow: 'USER_PASSWORD_AUTH',
+      ClientId: CLIENT_ID,
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: password
+      }
+    });
+
+    if (!AuthenticationResult) {
+      throw new Error();
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Successfully authenticated',
+        tokens: {
+          accessToken: AuthenticationResult.AccessToken,
+          idToken: AuthenticationResult.IdToken,
+          refreshToken: AuthenticationResult.RefreshToken
+        }
+      })
+    };
+  }
+  catch (error) {
+    console.error('Error:', error);
+    if (error && error instanceof Error) {
+      return {
+        statusCode: error.name === 'NotAuthorizedException' ? 401 : 500,
+        body: JSON.stringify({
+          message: error.message || 'Error during authentication'
+        })
+      }
+    }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Fatal Error'
+      })
+    };
+  }
 }
 
--- src/handlers/auth/signup.ts
+++ src/handlers/auth/signup.ts
@@ -1,9 +1,76 @@
+import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
 import { APIGatewayEvent, APIGatewayProxyHandler } from 'aws-lambda';
+import { emailRegex } from './util';
+
+const cognito = new CognitoIdentityProvider();
+const USER_POOL_ID = process.env.USER_POOL_ID!;
+const CLIENT_ID = process.env.USER_POOL_CLIENT_ID!;
+
+interface SignUpRequest {
+  email: string;
+  password: string;
+  name: string;
+}
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent) => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: JSON.stringify({ message: `Hello, world from ${event.path}` }),
+  try {
+    if (!event.body) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Missing request body' })
+      };
+    }
+    const { email, password }: SignUpRequest = JSON.parse(event.body);
+
+    if (!email || !password) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Missing required fields' })
+      };
+    }
+
+    if (!emailRegex.test(email)) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: `Invalid email of: ${email}` })
+      };
+    }
+
+    const signUpResult = await cognito.signUp({
+      ClientId: CLIENT_ID,
+      Username: email,
+      Password: password,
+      UserAttributes: [
+        { Name: 'email', Value: email },
+      ]
+    });
+
+
+    await cognito.adminConfirmSignUp({
+      UserPoolId: USER_POOL_ID,
+      Username: email
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'User registration successful',
+        userId: signUpResult.UserSub
+      }),
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    if (error && error instanceof Error) {
+      return {
+        statusCode: error.name === 'UsernameExistsException' ? 409 : 500,
+        body: JSON.stringify({
+          message: error.message || 'Error during registration'
+        })
+      };
+    }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Fatal Error' })
+    };
   };
 }
